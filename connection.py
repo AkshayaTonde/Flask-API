41af8718c62e469d885b62dd74c79486d318d07b
Akshaya Tonde <tondeakshaya@gmail.com>
2022-03-24 00:17:27 +0000

    connection string

--- connection.py
+++ connection.py
@@ -1,1 +1,19 @@
-#New file 
+from ipaddress import collapse_addresses
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://akshaya:akshaya1@cluster0.solpc.mongodb.net/CAOne?retryWrites=true&w=majority")
+
+db = cluster["CAOne"]
+colection = db["Users"]
+
+#to inser the document 
+#post = {"_id": 0, "name": "Leena", "username":"idleena", "email":"leena@gmail.com", "profilepict":" ","password": "123", "fav_Cat":["1" , "2"]}
+#colection.insert_one(post)
+
+#to read database 
+
+results = colection.find({"name":"Neha"})
+
+for result in results:
+    print(result)
