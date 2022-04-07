b7e585ef936d2aa4d6147046664ffd94390fd851
Enes <menesilhan@gmail.com>
2022-04-08 00:14:25 +0100

    fixed

--- connection.py
+++ connection.py
@@ -1,30 +1,37 @@
 from ipaddress import collapse_addresses
-import pymongo
-import flask
 from pymongo import MongoClient
-from flask import Flask
-from flask import request
+import pymongo
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
+from bson import ObjectId
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/") 
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+@app.route("/")
 def show():
-    cluster = MongoClient("mongodb+srv://akshaya:akshaya1@cluster0.solpc.mongodb.net/CAOne?retryWrites=true&w=majority")
+    client = MongoClient("mongodb://xlash:PWzJpIo975Q7hplS@cluster0-shard-00-00.solpc.mongodb.net:27017,cluster0-shard-00-01.solpc.mongodb.net:27017,cluster0-shard-00-02.solpc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-718qah-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.CAOne
+    collection = db["Users"]
 
-    db = cluster["CAOne"]
-    colection = db["Users"]
+    results = collection.find({})
 
-    # #to inser the document 
-    # #post = {"_id": 0, "name": "Leena", "username":"idleena", "email":"leena@gmail.com", "profilepict":" ","password": "123", "fav_Cat":["1" , "2"]}
-    # #colection.insert_one(post)
+    r_list = []
+    for r in results:
+        r_list.append(r)
+        print(r)
 
-    # #to read database 
-    results = colection.find({})
-
-    return flask.jsonify([result for result in results])
+    return JSONEncoder().encode({"results": r_list})
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0', port='8080', debug=True)
