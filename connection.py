227fb7eff0abc84364b50f994ff69f7cd7ae5bf8
Akshaya Tonde <tondeakshaya@gmail.com>
2022-04-07 23:53:36 +0100

    cursor to json

--- connection.py
+++ connection.py
@@ -1,33 +1,30 @@
 from ipaddress import collapse_addresses
-import re
 import pymongo
+import flask
 from pymongo import MongoClient
 from flask import Flask
 from flask import request
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
-@app.route("/")#URL leading to method
+CORS(app)
+@app.route("/") 
+def show():
+    cluster = MongoClient("mongodb+srv://akshaya:akshaya1@cluster0.solpc.mongodb.net/CAOne?retryWrites=true&w=majority")
 
-def ConnectionFunction():
+    db = cluster["CAOne"]
+    colection = db["Users"]
 
-  cluster = MongoClient("mongodb+srv://akshaya:akshaya1@cluster0.solpc.mongodb.net/CAOne?retryWrites=true&w=majority")
+    # #to inser the document 
+    # #post = {"_id": 0, "name": "Leena", "username":"idleena", "email":"leena@gmail.com", "profilepict":" ","password": "123", "fav_Cat":["1" , "2"]}
+    # #colection.insert_one(post)
 
-  db = cluster["CAOne"]
-  colection = db["Users"]
+    # #to read database 
+    results = colection.find({})
 
-  #to inser the document 
-  #post = {"_id": 0, "name": "Leena", "username":"idleena", "email":"leena@gmail.com", "profilepict":" ","password": "123", "fav_Cat":["1" , "2"]}
-  #colection.insert_one(post)
-
-  #to read database 
-
-  results = colection.find({"name":"Neha"})
-
-  return results
-
-  #for result in results:
-    #  print(result)
+    return flask.jsonify([result for result in results])
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080', ssl_context=('../../../home/akshaya/cert.pem', '../../../home/akshaya/privkey.pem')) 
+  app.run(host='0.0.0.0',port='8080')
